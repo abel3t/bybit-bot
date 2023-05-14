470ac802bf6dfe9969504b97850e9fe08533d97d
Abel Tran <abeltran.develop@gmail.com>
2023-05-14 18:18:44 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ dotEnv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8081);
 }
 bootstrap();
